f69bead3dd6b84ff06b4881067d8f85ffac91eee
Levente Kim <kimlevente@gmail.com>
2026-07-09 20:27:14 +0200

    fix snap budget ratio bug (wasn't taken into account during estimation)

---
+++ src/fair_dynamic_pricing_with_rl/utils/config.py
@@ -0,0 +1,10 @@
+#####
+### AIDS PARAMETERS
+#####
+
+# source: https://www.ers.usda.gov/data-products/foodaps-national-household-food-acquisition-and-purchase-survey/interactive-charts
+# (USDA, 2026)
+SNAP_BUDGET_RATIO = 35.7 / ((43.9 + 40.0)/2)
+# products with most different price points over history
+PRODUCT_LIST = ["FOODS_1_095","FOODS_1_096","FOODS_1_129"]
+CHOSEN_STATE = "TX"
