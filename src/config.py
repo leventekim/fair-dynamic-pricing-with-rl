9f86e1065c200e4590f4bd02aa3b99a0bb8d893b
Levente Kim <kimlevente@gmail.com>
2026-07-28 18:48:15 +0200

    saving eda figures using customizable path

---
+++ src/config.py
@@ -0,0 +1,19 @@
+###
+# STANDARD PARAMETERS
+###
+
+figures_path = "C:\\Users\\Kim Levente\\Documents\\01-projects\\research-project\\01_milestones\\03_final_report\\figures\\"
+
+###
+# EXPERIMENT 1 PARAMETERS
+###
+
+
+###
+# EXPERIMENT 2 PARAMETERS
+###
+
+
+###
+# EXPERIMENT 3 PARAMETERS
+###
