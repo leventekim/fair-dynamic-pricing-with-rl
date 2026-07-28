81b93a04cfb4427aa371f5e949cdc7c02370e462
Levente Kim <kimlevente@gmail.com>
2026-07-28 19:52:32 +0200

    experiment 2 iteration 1

--- src/config.py
+++ src/config.py
@@ -1,8 +1,37 @@
+import numpy as np
+
+
 ###
 # STANDARD PARAMETERS
 ###
 
-figures_path = "C:\\Users\\Kim Levente\\Documents\\01-projects\\research-project\\01_milestones\\03_final_report\\figures\\"
+FIGURES_PATH = "C:\\Users\\Kim Levente\\Documents\\01-projects\\research-project\\01_milestones\\03_final_report\\figures\\"
+
+# demand model parameters
+DEMAND_MODEL_PRICE_GRANULARITY = 0.001
+
+# MDP main parameters
+K = 5000  # number of episodes
+NUM_TIMESTEPS = 60  # number of time steps per episode
+NUM_PRODUCTS = 2
+
+# environment parameters
+P_MIN = np.array([5.0, 5.0])
+P_DIFF = np.array([0.01, 0.01])
+P_MAX = np.array([9.0, 9.0])
+N_SNAP_DAYS = 10
+CALENDAR_SEED = 42
+NOISE_RANGE = 1
+ENV_SEED = 42
+
+# PPO hyperparameters
+PPO_HYPERPARAMS = {
+    "learning_rate": 0.0001,
+    "batch_size": 64,  # default
+    "clip_range": 0.2,  # default
+    "seed": 42,
+}
+PPO_WARMUP_NR_EPISODES = 6000
 
 ###
 # EXPERIMENT 1 PARAMETERS
@@ -13,6 +42,7 @@ figures_path = "C:\\Users\\Kim Levente\\Documents\\01-projects\\research-project
 # EXPERIMENT 2 PARAMETERS
 ###
 
+FAIRNESS_WEIGHTS_LIST = [1.0, 0.7, 0.5, 0.0]
 
 ###
 # EXPERIMENT 3 PARAMETERS
--- src/fair_dynamic_pricing_with_rl/utils/fairness_utils.py
+++ src/fair_dynamic_pricing_with_rl/utils/fairness_utils.py
@@ -6,8 +6,8 @@ def compute_jains_index(action_hist, groups, p_min, p_diff):
     Compute Jain's fairness index across sensitive-attribute (SNAP-proxied) groups.
 
     Args:
-        action_hist (np.ndarray): raw action indices taken by the policy (T,K,L)
-        groups (np.ndarray): sensitive attribute labels (0/1) (T,K)
+        action_hist (np.ndarray): raw action indices taken by the policy (T,K,L) or (T,L)
+        groups (np.ndarray): sensitive attribute labels (0/1) (T,K) or (T)
         p_min (np.ndarray): minimum prices (L)
         p_diff (np.ndarray) price granularity (L)
 
@@ -18,19 +18,17 @@ def compute_jains_index(action_hist, groups, p_min, p_diff):
     unique_groups = np.unique(groups)
     group_values = {}
 
-    # get main parameters
-    T, K, L = action_hist.shape
+    # handle if action_hist was initialized to be (T,L) shaped
+    if action_hist.shape[0] != groups.shape[0]:
+        action_hist = action_hist[:groups.shape[0],:]
 
     # transform action indices to prices based on _action_to_price method
     # rounding guards against floating point inconsistencies
-    prices = np.zeros((T, K, L))
-    for product_idx in range(L):
-        prices[:, :, product_idx] = np.round(
-            p_min[product_idx] + action_hist[:, :, product_idx] * p_diff[product_idx],
-            10,
-        )
+    prices = np.zeros(action_hist.shape)
+    prices = np.round(p_min + action_hist * p_diff, 10)
 
-    total_price = prices.sum(axis=2)  # basket price per (t, k)
+    # basket price per (t, k) or (t), taking only 1-1 product
+    total_price = prices.sum(axis=-1)
 
     for g in unique_groups:
         mask = groups == g
